f531f90a24cc1159d81ffa076847fbf00d3b577b
Charley <charleyliu716@gmail.com>
2022-11-28 17:43:16 -0500

    Added noscript tags and functionality to header links

--- src/js/header.js
+++ src/js/header.js
@@ -4,9 +4,9 @@ template.innerHTML = `
 <nav id="navbar">
     <h1 id="title">Maze Solver</h1>
     <ul class="links">
-        <li><a href="">Home</a></li>
-        <li><a href="">About</a></li>
-        <li><a href="" target="_blank">Github</a></li>
+        <li><a href="/src/html/index.html">Home</a></li>
+        <li><a href="/src/html/about.html">About</a></li>
+        <li><a href="https://github.com/Columbium41" target="_blank">Github</a></li>
     </ul>
 </nav>
 `
