7076daaca908ceebf05331c37a6173a4f5c68ecf
Charley <charleyliu716@gmail.com>
2022-11-28 17:37:09 -0500

    Added reusability for navigation bar

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,14 @@
-console.log("header.js run");
+const template = document.createElement('template');
+
+template.innerHTML = `
+<nav id="navbar">
+    <h1 id="title">Maze Solver</h1>
+    <ul class="links">
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="" target="_blank">Github</a></li>
+    </ul>
+</nav>
+`
+
+document.body.insertBefore(template.content, document.body.firstChild);
