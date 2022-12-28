88d5c31ecdaa6a4d0043369b2c8775b3cb9d6a1d
Charley Liu <charleyliu716@gmail.com>
2022-12-28 17:38:38 -0500

    Fixed navbar links

--- src/js/header.js
+++ src/js/header.js
@@ -4,8 +4,8 @@ template.innerHTML = `
 <nav id="navbar">
     <h1 id="title">Maze Solver</h1>
     <ul class="links">
-        <li><a href="./src/html/index.html">Home</a></li>
-        <li><a href="./src/html/about.html">About</a></li>
+        <li><a href="./index.html">Home</a></li>
+        <li><a href="./about.html">About</a></li>
         <li><a href="https://github.com/Columbium41" target="_blank">Github</a></li>
     </ul>
 </nav>
